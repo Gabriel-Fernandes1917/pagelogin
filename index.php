1c5cd42008bde9523e350f4d0743d9cc364eb3eb
Gabriel Fernandes <gabgui2001@gmail.com>
2021-07-30 12:06:53 -0300

    finished front-end the login page

--- index.php
+++ index.php
@@ -17,13 +17,22 @@
         <main>
             
             <section class="loginsection">
-            <center>
-            <input type="text" name="name" required class="userdatabase">
-            <br>
-            <input type="email" name="email" required class="userdatabase">
-            <br>
-            <input type="date" name="born" required class="userdatabase">
-            </center>
+                <center>
+                    <img src="">
+                    <h2>Make your Sing-in or Login</h2>
+                    <br>
+                    <input type="text" name="name" required class="userdatabase" placeholder="Your name"> 
+                    <br>
+                    <input type="email" name="email" required class="userdatabase" placeholder="Your best e-mail">
+                    <br>
+                    <input type="date" name="born" required class="userdatabase" placeholder="Your born date">
+                    <br>
+
+                    <div class="btns">
+                        <input type="button" name="btnlogin" class="btnlogin" value="Login">
+                        <input type="button" name="btnsingin" class="btnsingin" value="Sing-in">
+                    </div>
+                </center>
             </section>
 
         </main>
